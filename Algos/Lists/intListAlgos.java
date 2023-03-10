70523d9e25f4a8ed2a1bb41a6707c791da06b39d
ARandomCow <107651457+ARandomCow@users.noreply.github.com>
2023-03-09 19:18:49 -0700

    Update intListAlgos.java

--- Algos/Lists/intListAlgos.java
+++ Algos/Lists/intListAlgos.java
@@ -15,60 +15,65 @@ intPerms.permutations(intsList, new ArrayList<Integer>());
 Integer[][] allIntPerms = intPerms.getAllPerms();
 */
 
-    public static class intListAlgos 
-    {
-        int number;
-        Integer[][] allPerms;
-        int size;
+public static class intListAlgos 
+{
+    int number;
+    Integer[][] allPerms;
+    int size;
 
 
     //input = integer n
     //output = n!
 
-    public static int factorial(int n){
+    public static int factorial(int n)
+    {
         int result = n;
-        for (int i = n-1; i>0; i--){
+        for (int i = n-1; i>0; i--)
+        {
             result *= i;
         }
-            return result;
-        }
+        return result;
+    }
         
        
-        public intListAlgos(int size){
-            allPerms = new Integer[factorial(size)][];
-            this.size = size;
-            number = 0;
-        }
-        // Generates all permutations -- EXAMPLE IN listAlgoTesting.java
-        public void permutations(List<Integer> items, List<Integer> permutation) {
-
-            /* permutation stack has become equal to size that we require */
-            if(permutation.size() == size) {
-                /* print the permutation */
-//                System.out.println(Arrays.toString(permutation.toArray(new Integer[0])));
-                allPerms[number] = permutation.toArray(new Integer[0]);
-                number++;
-            }
-
-            /* items available for permutation */
-            Integer[] availableItems = items.toArray(new Integer[0]);
-            for(Integer i : availableItems) {
-                /* add current item */
-                permutation.add(i);
-
-                /* remove item from available item set */
-                items.remove(i);
-
-                /* pass it on for next permutation */
-                permutations(items, permutation);
-
-                /* pop and put the removed item back */
-                items.add(permutation.remove(permutation.size()-1));
-            }
-        }
-
-        public Integer[][] getAllPerms(){
-            return allPerms;
-        }
-
+    public intListAlgos(int size)
+    {
+        allPerms = new Integer[factorial(size)][];
+        this.size = size;
+        number = 0;
     }
+        // Generates all permutations -- EXAMPLE IN listAlgoTesting.java
+    public void permutations(List<Integer> items, List<Integer> permutation) 
+    {
+
+        /* permutation stack has become equal to size that we require */
+        if(permutation.size() == size) 
+        {
+            /* adds the permutation to the eventual return array */
+            allPerms[number] = permutation.toArray(new Integer[0]);
+            number++;
+        }
+
+        /* items available for permutation */
+        Integer[] availableItems = items.toArray(new Integer[0]);
+        for(Integer i : availableItems) 
+        {
+            /* add current item */
+            permutation.add(i);
+
+            /* remove item from available item set */
+            items.remove(i);
+
+            /* pass it on for next permutation */
+            permutations(items, permutation);
+
+            /* pop and put the removed item back */
+            items.add(permutation.remove(permutation.size()-1));
+        }
+    }
+
+    public Integer[][] getAllPerms()
+    {
+        return allPerms;
+    }
+}
